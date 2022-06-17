1767f79e7a26d3de2222f68bdc8b4e47bc9c7b7c
Umang <umangagarwal345@gmail.com>
2022-06-17 22:26:11 +0530

    Added endpoint for verification token account activation

--- src/main/java/com/umang345/redditclonebackend/controller/AuthController.java
+++ src/main/java/com/umang345/redditclonebackend/controller/AuthController.java
@@ -5,10 +5,7 @@ import com.umang345.redditclonebackend.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /***
  *  Rest Controller for authentication endpoints
@@ -25,5 +22,12 @@ public class AuthController
     {
         authService.signup(registerRequest);
         return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
+    }
+
+    @GetMapping("/accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token)
+    {
+         authService.verifyAccount(token);
+         return new ResponseEntity<>("Account activated successfully.", HttpStatus.OK);
     }
 }
