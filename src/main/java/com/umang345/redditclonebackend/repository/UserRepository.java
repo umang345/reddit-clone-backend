87b29c5869cc330df46112dcb82f4a2cfce69839
Umang <umangagarwal345@gmail.com>
2022-06-17 15:37:26 +0530

    Added JPARepository interfaces for User and VerificationToken entities

---
+++ src/main/java/com/umang345/redditclonebackend/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.umang345.redditclonebackend.repository;
+
+import com.umang345.redditclonebackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
