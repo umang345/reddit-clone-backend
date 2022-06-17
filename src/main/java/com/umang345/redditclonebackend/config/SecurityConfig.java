3f31e7f68cdfa5ad2bc22f9bb4922bebfc403d00
Umang <umangagarwal345@gmail.com>
2022-06-17 15:33:09 +0530

    Added Spring security config class. Configured routes for auth endpoints and added bean for password encoder

---
+++ src/main/java/com/umang345/redditclonebackend/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.umang345.redditclonebackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/***
+ * Security Configuration for Backend
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers("/api/auth/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated();
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEncoder()
+    {
+         return new BCryptPasswordEncoder();
+    }
+}
