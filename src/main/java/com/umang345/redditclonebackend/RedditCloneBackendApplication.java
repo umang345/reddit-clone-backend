5d675d0c993a571836afa0f20eff1ad80b0eb9de
Umang <umangagarwal345@gmail.com>
2022-06-20 11:21:32 +0530

    Added Swagger UI for documenting Rest APIs

--- src/main/java/com/umang345/redditclonebackend/RedditCloneBackendApplication.java
+++ src/main/java/com/umang345/redditclonebackend/RedditCloneBackendApplication.java
@@ -1,11 +1,14 @@
 package com.umang345.redditclonebackend;
 
+import com.umang345.redditclonebackend.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class RedditCloneBackendApplication {
 
 	public static void main(String[] args) {
